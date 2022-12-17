f668fe2a36085513304d572a56c72a41aeeedf2d
Moin97 <moinuddin9777@gmail.com>
2022-12-17 08:00:00 +0530

    implemented widgets

---
+++ lib/screens/prediction_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:mnistdigitrecognizer/models/prediction.dart';
+
+class PredictionWidget extends StatelessWidget {
+  final List<Prediction> predictions;
+
+  const PredictionWidget({Key key, this.predictions}) : super(key: key);
+
+  Widget _numberWidget(int num, Prediction prediction) {
+    return Column(
+      children: <Widget>[
+        Text(
+          '$num',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: prediction == null
+                ? Colors.grey[350]
+                : Colors.amber.withOpacity(
+                    (prediction.confidence * 2).clamp(0, 1).toDouble(),
+                  ),
+          ),
+        ),
+        Text(
+          '${prediction == null ? '' : prediction.confidence.toStringAsFixed(3)}',
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  List<dynamic> getPredictionStyles(List<Prediction> predictions) {
+    List<dynamic> data = [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ];
+    predictions?.forEach((prediction) {
+      data[prediction.index] = prediction;
+    });
+
+    return data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var styles = getPredictionStyles(this.predictions);
+
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            for (var i = 0; i < 5; i++) _numberWidget(i, styles[i])
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            for (var i = 5; i < 10; i++) _numberWidget(i, styles[i])
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// class MyPredictionWidget extends StatefulWidget {
+//   const MyPredictionWidget({Key key}) : super(key: key);
+
+//   @override
+//   State<MyPredictionWidget> createState() => MyPredictionWidgetState();
+// }
+
+// class MyPredictionWidgetState extends State<MyPredictionWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ;
+//   }
+// }
